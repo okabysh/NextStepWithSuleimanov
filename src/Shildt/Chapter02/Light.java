badc6af228e4decdeafec5ddc72b8154fcffaa4c
Oleg Kabysh <okabysh@gmail.com>
2017-03-20 21:57:59 +0200

    mod.2

---
+++ src/Shildt/Chapter02/Light.java
@@ -0,0 +1,26 @@
+package Shildt.Chapter02;
+
+/**
+ * Created by Oleg Kabysh on 20.03.2017.
+ */
+public class Light {
+    public static void main(String[] args) {
+        int lightspeed;
+        long days;
+        long seconds;
+        long distance;
+
+        // скорость света, миль в секунду
+        lightspeed = 186000;
+
+        days = 1000;
+
+        seconds = days * 24 * 60 * 60;
+
+        distance = lightspeed * seconds;
+
+        System.out.print("За " + days);
+        System.out.print(" дней свет пройдет около ");
+        System.out.println(distance + " миль.");
+    }
+}
