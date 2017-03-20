40dd7b6cf371503232f83c7c23e822c9a2b9a728
Oleg Kabysh <okabysh@gmail.com>
2017-03-20 22:10:42 +0200

    mod.2

---
+++ src/Shildt/Chapter02/CharDemo.java
@@ -0,0 +1,16 @@
+package Shildt.Chapter02;
+
+/**
+ * Created by Oleg Kabysh on 20.03.2017.
+ */
+public class CharDemo {
+    public static void main(String[] args) {
+        char ch1, ch2;
+
+        ch1 = 88;
+        ch2 = 'Y';
+
+        System.out.print("ch1 и ch2: ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
