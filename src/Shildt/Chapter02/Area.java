cef97be836aa5bf198e199c5d528af5ecf375012
Oleg Kabysh <okabysh@gmail.com>
2017-03-20 22:03:36 +0200

    mod.2

---
+++ src/Shildt/Chapter02/Area.java
@@ -0,0 +1,16 @@
+package Shildt.Chapter02;
+
+/**
+ * Created by Oleg Kabysh on 20.03.2017.
+ */
+public class Area {
+    public static void main(String[] args) {
+        double pi, r, a;
+
+        r = 10.8;
+        pi = 3.1416;
+        a = pi * r * r;
+
+        System.out.println(a);
+    }
+}
