f77bb0a39066dd0726d9ff215bc5b4926d89746b
Oleg Kabysh <okabysh@gmail.com>
2017-03-20 21:46:33 +0200

    mod.1

---
+++ .idea/src/Shildt/Chapter02/IfSample.java
@@ -0,0 +1,23 @@
+package HerbertSchildt.Chapter01;
+
+/**
+ * Created by Oleg Kabysh on 15.03.2017.
+ */
+public class IfSample {
+    public static void main(String[] args) {
+        int x,y;
+
+        x = 10;
+        y = 20;
+
+        if (x < y) System.out.println("x меньше y");
+
+        x = x*2;
+        if (x == y) System.out.println("x теперь равно y");
+
+        x = x*2;
+        if (x > y) System.out.println("x теперь больше y");
+
+        if (x == y) System.out.println("Вы не увидите этого!");
+    }
+}
