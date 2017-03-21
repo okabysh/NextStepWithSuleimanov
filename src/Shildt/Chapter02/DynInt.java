bc9cfca53fc327c1c7f7185a0f432526c7bb9620
Oleg Kabysh <okabysh@gmail.com>
2017-03-21 15:33:01 +0200

    mod.4 from 21.03.2017

---
+++ src/Shildt/Chapter02/DynInt.java
@@ -0,0 +1,13 @@
+package Shildt.Chapter02;
+
+/**
+ * Created by kabysh_ol on 21.03.2017.
+ */
+public class DynInt {
+    public static void main(String[] args) {
+        double a = 3.0, b = 4.0;
+        // динамическая инициализация переменной с
+        double c = Math.sqrt(a*a + b*b);
+        System.out.println("Гипотенуза равна: " + c);
+    }
+}
