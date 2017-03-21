34c0f1e1138c0be7accafe25638df39324071386
Oleg Kabysh <okabysh@gmail.com>
2017-03-21 16:22:46 +0200

    mod.7 from 21.03.2017

---
+++ src/HerbertSсhildt/Chapter03/Conversion.java
@@ -0,0 +1,25 @@
+package HerbertSсhildt.Chapter03;
+
+/**
+ * Created by kabysh_ol on 21.03.2017.
+ */
+public class Conversion {
+    public static void main(String[] args) {
+        byte b;
+        int i = 128;
+        double d = 323.142;
+
+        System.out.println("\nПреобразование типа int в тип byte.");
+        b = (byte) i;
+        System.out.println("i и b " + i + " " + b);
+
+        System.out.println("\nПреобразование типа double в тип int.");
+        i = (int) d;
+        System.out.println("d и i " + d + " " + i);
+
+        System.out.println("\nПреобразование типа double в тип byte.");
+        b = (byte) d;
+        System.out.println("d и b " + d + " " + b);
+    }
+
+}
