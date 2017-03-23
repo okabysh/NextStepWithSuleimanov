85e3598c7dfd441334f413b2591be77b5bb01aa3
Oleg Kabysh <okabysh@gmail.com>
2017-03-23 17:34:35 +0200

    modification from 23.03.2017

---
+++ src/HerbertShildt/Chapter04/HexByte.java
@@ -0,0 +1,17 @@
+package HerbertShildt.Chapter04;
+
+/**
+ * Created by kabysh_ol on 23.03.2017.
+ */
+public class HexByte {
+    public static void main(String[] args) {
+        char hex[] = {
+            '0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+        };
+
+        byte b = (byte) 0xf1;
+
+        System.out.println("b = 0x" + hex[(b >> 4) & 0x0f] + hex[b & 0x0f]);
+    }
+}
