1ca6c6f0bb87538cf40057dd3d46823538548deb
Oleg Kabysh <okabysh@gmail.com>
2017-03-23 20:55:41 +0200

    modification from 23.03.2017

---
+++ src/HerbertShildt/Chapter04/Ternary.java
@@ -0,0 +1,21 @@
+package HerbertShildt.Chapter04;
+
+/**
+ * Created by Oleg Kabysh on 23.03.2017.
+ */
+public class Ternary {
+    public static void main(String[] args) {
+        int i, k;
+
+        i = 10;
+        k = i < 0 ? -i : i;
+        System.out.print("Абсолютное значение ");
+        System.out.println(i + " равно " + k);
+
+        i = -10;
+        k = i < 0 ? -i : i;
+        System.out.print("Абсолютное значение ");
+        System.out.println(i + " равно " + k);
+
+    }
+}
