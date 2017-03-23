b81b8fb998c5083683995771aff4c7004f6fcc83
Oleg Kabysh <okabysh@gmail.com>
2017-03-23 20:44:25 +0200

    modification from 23.03.2017

---
+++ src/HerbertShildt/Chapter04/BoolDiv.java
@@ -0,0 +1,16 @@
+package HerbertShildt.Chapter04;
+
+/**
+ * Created by Oleg Kabysh on 23.03.2017.
+ */
+public class BoolDiv {
+    public static void main(String[] args) {
+        int num = 10;
+        int denom = 0;
+        if (denom != 0 && num/denom > 0) {
+            System.out.println("then");
+        } else {
+            System.out.println("else");
+        }
+    }
+}
