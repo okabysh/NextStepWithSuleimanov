c26e15687ab26b5f95e55484d9a3172ab90a44d5
Oleg Kabysh <okabysh@gmail.com>
2017-03-23 23:47:25 +0200

    modification from 23.03.2017

---
+++ src/HerbertShildt/Chapter05/StringSwitch.java
@@ -0,0 +1,24 @@
+package HerbertShildt.Chapter05;
+
+/**
+ * Created by Oleg Kabysh on 23.03.2017.
+ */
+public class StringSwitch {
+    public static void main(String[] args) {
+        String str = "два";
+        switch (str) {
+            case "один":
+                System.out.println("один");
+                break;
+            case "два":
+                System.out.println("два");
+                break;
+            case "три":
+                System.out.println("три");
+                break;
+            default:
+                System.out.println("не совпало!");
+
+        }
+    }
+}
