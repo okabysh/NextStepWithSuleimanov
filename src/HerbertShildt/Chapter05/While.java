2e029956a630435a09916be3c9ba84ded6942559
Oleg Kabysh <okabysh@gmail.com>
2017-03-23 23:54:07 +0200

    modification from 23.03.2017

---
+++ src/HerbertShildt/Chapter05/While.java
@@ -0,0 +1,14 @@
+package HerbertShildt.Chapter05;
+
+/**
+ * Created by Oleg Kabysh on 23.03.2017.
+ */
+public class While {
+    public static void main(String[] args) {
+        int n = 10;
+        while (n > 0) {
+            System.out.println("такт " + n);
+            n--;
+        }
+    }
+}
