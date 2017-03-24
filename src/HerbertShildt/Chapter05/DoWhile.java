b79c2b49648fe0e256a1aa7cf81610372eaceab0
Oleg Kabysh <okabysh@gmail.com>
2017-03-24 14:21:21 +0200

    modification from 23.03.2017

---
+++ src/HerbertShildt/Chapter05/DoWhile.java
@@ -0,0 +1,17 @@
+package HerbertShildt.Chapter05;
+
+/**
+ * Created by kabysh_ol on 24.03.2017.
+ */
+public class DoWhile {
+    public static void main(String[] args) {
+        int n = 10;
+//        do {
+//            System.out.println(" такт " + n);
+//            n--;
+//        } while (n > 0);
+        do {
+            System.out.println(" такт " + n);
+        } while (--n > 0);
+    }
+}
