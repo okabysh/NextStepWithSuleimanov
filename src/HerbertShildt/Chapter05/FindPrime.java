0ecdbb54541923554094903c5fbb0174bec3907b
Oleg Kabysh <okabysh@gmail.com>
2017-03-24 16:57:24 +0200

    modification from 24.03.2017

---
+++ src/HerbertShildt/Chapter05/FindPrime.java
@@ -0,0 +1,25 @@
+package HerbertShildt.Chapter05;
+
+/**
+ * Created by kabysh_ol on 24.03.2017.
+ */
+public class FindPrime {
+    public static void main(String[] args) {
+        int num;
+        boolean isPrime;
+
+        num = 14;
+
+        if (num < 2) isPrime = false;
+        else isPrime = true;
+
+        for (int i=2; i<= num/i; i++) {
+            if ((num % i) == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) System.out.println("Простое число");
+        else System.out.println("Не простое число");
+    }
+}
