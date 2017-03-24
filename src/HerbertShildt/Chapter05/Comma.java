8dacb4f2c4508917399957de78a8e4d706600bf7
Oleg Kabysh <okabysh@gmail.com>
2017-03-24 17:05:08 +0200

    modification from 24.03.2017

---
+++ src/HerbertShildt/Chapter05/Comma.java
@@ -0,0 +1,14 @@
+package HerbertShildt.Chapter05;
+
+/**
+ * Created by kabysh_ol on 24.03.2017.
+ */
+public class Comma {
+    public static void main(String[] args) {
+        int a, b;
+        for (a=1, b=4; a<b; a++, b--) {
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+        }
+    }
+}
