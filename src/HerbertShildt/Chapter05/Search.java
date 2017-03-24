9b6f1082aaaaa9c05767294d2daa43ca190904ac
Oleg Kabysh <okabysh@gmail.com>
2017-03-24 17:40:25 +0200

    modification from 24.03.2017

---
+++ src/HerbertShildt/Chapter05/Search.java
@@ -0,0 +1,23 @@
+package HerbertShildt.Chapter05;
+
+/**
+ * Created by kabysh_ol on 24.03.2017.
+ */
+public class Search {
+    public static void main(String[] args) {
+        int nums[] = {6, 8, 3, 7, 5, 6, 1, 4};
+        int val = 5;
+        boolean found = false;
+
+        for (int x :
+                nums) {
+            if (x == val) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found)
+            System.out.println("Значение найдено!");
+    }
+}
