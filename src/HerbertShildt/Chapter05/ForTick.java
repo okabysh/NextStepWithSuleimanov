20c870d5dd2ba7a2479a4e7792e581b0ba23dd74
Oleg Kabysh <okabysh@gmail.com>
2017-03-24 16:38:22 +0200

    modification from 24.03.2017

---
+++ src/HerbertShildt/Chapter05/ForTick.java
@@ -0,0 +1,12 @@
+package HerbertShildt.Chapter05;
+
+/**
+ * Created by kabysh_ol on 24.03.2017.
+ */
+public class ForTick {
+    public static void main(String[] args) {
+        int n;
+        for (n=10; n>0; n--)
+            System.out.println("такт " + n);
+    }
+}
