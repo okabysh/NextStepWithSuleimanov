9d2ccdd02371536fd66d8669182e0bbbc25929b2
Oleg Kabysh <okabysh@gmail.com>
2017-03-24 17:35:31 +0200

    modification from 24.03.2017

---
+++ src/HerbertShildt/Chapter05/ForEach3.java
@@ -0,0 +1,24 @@
+package HerbertShildt.Chapter05;
+
+/**
+ * Created by kabysh_ol on 24.03.2017.
+ */
+public class ForEach3 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int nums[][] = new int[3][5];
+
+        for(int i=0; i<3; i++) {
+            for (int j=0; j<5; j++) {
+                nums[i][j] = (i+1)*(j+1);
+            }
+        };
+        for (int x[] : nums) {
+            for (int y : x) {
+                System.out.println("Значение = " + y);
+                sum += y;
+            }
+        }
+        System.out.println("Сумма = " + sum);
+    }
+}
