2bbb23d3fa9be7b7dd129237967d2133bb382347
Oleg Kabysh <okabysh@gmail.com>
2017-03-25 23:13:49 +0200

    modification from 25.03.2017

--- src/HerbertShildt/Chapter06/BoxDemo2.java
+++ src/HerbertShildt/Chapter06/BoxDemo2.java
@@ -9,17 +9,17 @@ public class BoxDemo2 {
         Box mybox2 = new Box();
         double vol;
 
-        mybox1.weight = 10;
+        mybox1.width = 10;
         mybox1.height = 20;
         mybox1.depth = 15;
 
-        mybox2.weight = 3;
+        mybox2.width = 3;
         mybox2.height = 6;
         mybox2.depth = 9;
 
-        vol = mybox1.weight * mybox1.height * mybox1.depth;
+        vol = mybox1.width * mybox1.height * mybox1.depth;
         System.out.println("Volume1 = " + vol);
-        vol = mybox2.weight * mybox2.height * mybox2.depth;
+        vol = mybox2.width * mybox2.height * mybox2.depth;
         System.out.println("Volume2 = " + vol);
     }
 }
