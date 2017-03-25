103156731a5876f11867dc8538e9b75b8fb509cc
Oleg Kabysh <okabysh@gmail.com>
2017-03-25 23:23:50 +0200

    modification from 25.03.2017

---
+++ src/HerbertShildt/Chapter06/BoxDemo4.java
@@ -0,0 +1,25 @@
+package HerbertShildt.Chapter06;
+
+/**
+ * Created by Oleg Kabysh on 25.03.2017.
+ */
+public class BoxDemo4 {
+    public static void main(String[] args) {
+        Box mybox1 = new Box();
+        Box mybox2 = new Box();
+        double vol;
+
+        mybox1.width = 10;
+        mybox1.height = 20;
+        mybox1.depth = 15;
+
+        mybox2.width = 3;
+        mybox2.height = 6;
+        mybox2.depth = 9;
+
+        vol = mybox1.volume();
+        System.out.println("Volume1 = " + vol);
+        vol = mybox2.volume();
+        System.out.println("Volume2 = " + vol);
+    }
+}
