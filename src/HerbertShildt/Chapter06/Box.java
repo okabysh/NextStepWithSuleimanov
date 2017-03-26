cfc68b8d8a99144d69797bae8c728b966282251b
Oleg Kabysh <okabysh@gmail.com>
2017-03-26 02:14:06 +0200

    modification from 26.03.2017

--- src/HerbertShildt/Chapter06/Box.java
+++ src/HerbertShildt/Chapter06/Box.java
@@ -8,16 +8,26 @@ public class Box {
     double height;
     double depth;
 
-    double volume() {
-//        System.out.print("Volume = ");
-//        System.out.println(width * height * depth);
-        return width * height * depth;
-    }
-
     public Box(double w, double h, double d) {
         width = w;
         height = h;
         depth = d;
+    }
+
+    public Box() {
+        width = -1;
+        height = -1;
+        depth = -1;
+    }
+
+    public Box(double len) {
+        width = height = depth = len;
+    }
+
+    public double volume() {
+//        System.out.print("Volume = ");
+//        System.out.println(width * height * depth);
+        return width * height * depth;
     }
 
 }
