ff685621ba0d86f4132674cb5f8fc37d0c87b05d
Oleg Kabysh <okabysh@gmail.com>
2017-03-27 17:08:35 +0300

    modification from 27.03.2017

---
+++ src/HerbertShildt/Chapter07/StringDemo3.java
@@ -0,0 +1,14 @@
+package HerbertShildt.Chapter07;
+
+/**
+ * Created by kabysh_ol on 27.03.2017.
+ */
+public class StringDemo3 {
+    public static void main(String[] args) {
+        String str[] = {"one", "two", "three"};
+        for (String i :
+                str) {
+            System.out.println(i);
+        }
+    }
+}
