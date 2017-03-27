7f140b7eda884670e3b22cc486e8b586d7aae44a
Oleg Kabysh <okabysh@gmail.com>
2017-03-27 13:57:25 +0300

    modification from 27.03.2017

---
+++ src/HerbertShildt/Chapter07/UseStatic.java
@@ -0,0 +1,25 @@
+package HerbertShildt.Chapter07;
+
+/**
+ * Created by kabysh_ol on 27.03.2017.
+ */
+public class UseStatic {
+    static int a = 3;
+    static  int b;
+
+    static void meth(int x) {
+        System.out.println("x = " + x);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+    static {
+        System.out.println("Static block is initialized");
+        b = a * 4;
+    }
+
+    public static void main(String[] args) {
+        meth(42);
+    }
+
+}
