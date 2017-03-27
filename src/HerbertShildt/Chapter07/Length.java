6ac178c71ad3668cd53b0400982c452d9c06b4ce
Oleg Kabysh <okabysh@gmail.com>
2017-03-27 14:10:22 +0300

    modification from 27.03.2017

---
+++ src/HerbertShildt/Chapter07/Length.java
@@ -0,0 +1,16 @@
+package HerbertShildt.Chapter07;
+
+/**
+ * Created by kabysh_ol on 27.03.2017.
+ */
+public class Length {
+    public static void main(String[] args) {
+        int a1[] = new int[10];
+        int a2[] = {3, 5, 7, 1, 8, 99, 44, -10};
+        int a3[] = {4, 3, 2, 1};
+
+        System.out.println("длинна a1 = " + a1.length);
+        System.out.println("длинна a2 = " + a2.length);
+        System.out.println("длинна a3 = " + a3.length);
+    }
+}
