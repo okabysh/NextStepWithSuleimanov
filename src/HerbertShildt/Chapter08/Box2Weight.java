5057b5faaa76041d5b518228eb53053d64c2f0ad
Oleg Kabysh <okabysh@gmail.com>
2017-03-28 21:51:31 +0300

    modification from 28.03.2017

--- src/HerbertShildt/Chapter08/Box2Weight.java
+++ src/HerbertShildt/Chapter08/Box2Weight.java
@@ -7,9 +7,10 @@ public class Box2Weight extends Box2 {
     double weight;
 
     Box2Weight(double w, double h, double d, double m) {
-        width = w;
-        height = h;
-        depth = d;
+//        width = w;
+//        height = h;
+//        depth = d;
+        super(w, h, d);
         weight = m;
     }
 }
