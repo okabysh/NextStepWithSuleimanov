6d6c0da6986a3a68a26dc6a2de0c45f66afb25a8
Oleg Kabysh <okabysh@gmail.com>
2017-03-29 22:54:36 +0300

    modification from 29.03.2017

---
+++ src/HerbertShildt/Chapter08/UseSuper.java
@@ -0,0 +1,11 @@
+package HerbertShildt.Chapter08;
+
+/**
+ * Created by Oleg Kabysh on 29.03.2017.
+ */
+public class UseSuper {
+    public static void main(String[] args) {
+        B_3 subOb = new B_3(1, 2);
+        subOb.show();
+    }
+}
