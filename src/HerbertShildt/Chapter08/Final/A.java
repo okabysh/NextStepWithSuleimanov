b53297bcfea8f0ca302a8407bb9cccc829612ea3
Oleg Kabysh <okabysh@gmail.com>
2017-03-30 14:48:38 +0300

    modification from 30.03.2017

---
+++ src/HerbertShildt/Chapter08/Final/A.java
@@ -0,0 +1,10 @@
+package HerbertShildt.Chapter08.Final;
+
+/**
+ * Created by kabysh_ol on 30.03.2017.
+ */
+public class A {
+    final void meth() {
+        System.out.println("It is end method");
+    }
+}
---
+++ src/HerbertShildt/Chapter08/Final/B.java
@@ -0,0 +1,8 @@
+package HerbertShildt.Chapter08.Final;
+
+/**
+ * Created by kabysh_ol on 30.03.2017.
+ */
+public class B extends A {
+    // void meth(); нельзя!
+}
