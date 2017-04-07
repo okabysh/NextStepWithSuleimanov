95b28a584fd126c8933b14c9aee593560d916f07
Oleg Kabysh <okabysh@gmail.com>
2017-04-07 21:30:40 +0300

    modification from 07.04.2017

---
+++ src/Golovach/Lecture02/ArrayBinarySearch.java
@@ -0,0 +1,22 @@
+package Golovach.Lecture02;
+
+import java.util.Arrays;
+
+/**
+ * Created by Oleg Kabysh on 07.04.2017.
+ */
+public class ArrayBinarySearch {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40};
+        System.out.println(Arrays.binarySearch(array, 5));
+        System.out.println(Arrays.binarySearch(array, 10));
+        System.out.println(Arrays.binarySearch(array, 15));
+        System.out.println(Arrays.binarySearch(array, 20));
+        System.out.println(Arrays.binarySearch(array, 25));
+        System.out.println(Arrays.binarySearch(array, 30));
+        System.out.println(Arrays.binarySearch(array, 35));
+        System.out.println(Arrays.binarySearch(array, 40));
+        System.out.println(Arrays.binarySearch(array, 45));
+        System.out.println(Arrays.binarySearch(array, 50));
+    }
+}
